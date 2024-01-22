c54af6331791f64b103d52ad57589982d2d4d466
omaymaelbekri <elbekriomayma@gmail.com>
2024-01-22 15:58:21 -0800

    moood

--- src/main/java/org/techlab/labxpert/dtos/AnalyseDTO.java
+++ src/main/java/org/techlab/labxpert/dtos/AnalyseDTO.java
@@ -1,22 +1,31 @@
 package org.techlab.labxpert.dtos;
 
 import lombok.Data;
+
 import org.techlab.labxpert.entity.Echantillon;
 import org.techlab.labxpert.entity.ReactifAnalyse;
 
 import javax.persistence.Column;
 import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
 import java.util.Collection;
 import java.util.Date;
 
 @Data
 public class AnalyseDTO {
     private Long idAnalyse;
+    @NotNull(message="nom null")
     private Echantillon echantillon;
+    @NotBlank(message="nom null")
     private String nomAnalyse;
+    @NotNull(message="nom null")
     private Date dateDebut;
+    @NotNull(message="nom null")
     private Date dateFin;
+    @NotBlank(message="nom null")
     private String commantaire;
     private Collection<ReactifAnalyse> reactifAnalyseList;
-    private Boolean deleted=false;
+    private boolean deleted;
 }
--- src/main/java/org/techlab/labxpert/dtos/FournisseurDTO.java
+++ src/main/java/org/techlab/labxpert/dtos/FournisseurDTO.java
@@ -2,11 +2,18 @@ package org.techlab.labxpert.dtos;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
 @Data
 public class FournisseurDTO {
     private Long idFournisseur;
+    @NotBlank(message="nom null")
     private String nom;
+    @NotBlank(message="nom null")
     private String adresse;
+    @Pattern(regexp = "^\\d{10}$", message = "Invalid phone number")
     private String tel;
-    private Boolean deleted=false;
+
+    private boolean deleted;
 }
